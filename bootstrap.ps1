41bcbe479633ab5550e0ea93f71166981d582899
Johannes Schneider <johannes-schneider_1995@web.de>
2022-05-27 16:08:56 +0200

    improve bootstrap convenience

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,11 +1,27 @@
-python -m venv .venv
-./.venv/Scripts/activate.ps1
+python -m venv .venv > $null
+./.venv/Scripts/activate.ps1 > $null
 
-pip install -r requirements.txt
+pip install -r requirements.txt > $null
 
+# create dev-scripts from template
 $currentDir = $(Get-Location).toString().Replace("\", "/")
 $content = Get-Content "./template.dev-scripts.ps1"
 $content[0] = "`$__scripts_path = `"$currentDir`""
 Set-Content "./dev-scripts.ps1" $content
 
-deactivate
+# update user profile
+$profileContent = Get-Content $PROFILE
+$loadDevScripts = ". `"$currentDir/dev-scripts.ps1`""
+if ( -not ($profileContent -contains $loadDevScripts)) {
+    if ($profileContent.Length -gt 0) {
+        $profileContent += ""
+    }
+    $profileContent += "###########################"
+    $profileContent += "# Loading dev-env scripts #"
+    $profileContent += $loadDevScripts
+    $profileContent += "###########################"
+
+    Set-Content $PROFILE $profileContent
+}
+
+deactivate > $null
