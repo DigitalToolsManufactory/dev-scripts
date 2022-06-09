289e7c2412f84cfd5f876be24b9738e6068f8c8c
Johannes Schneider <johannes-schneider_1995@web.de>
2022-06-09 08:38:24 +0200

    gch: don't raise if the current branch cannot be determined

--- gch.py
+++ gch.py
@@ -88,20 +88,26 @@ def _get_head_branch(dev_env: DevelopmentEnvironment, remote_name: Optional[str]
 
 
 def _get_current_branch(dev_env: DevelopmentEnvironment) -> Optional[CurrentLocalBranch]:
-    branch_response: ShellResponse = dev_env.shell.run_or_raise("git", ["branch"], dev_env.root)
+    branch_response: ShellResponse = dev_env.shell.run("git", ["branch"], dev_env.root)
+
+    if not branch_response.is_success:
+        return None
 
     current_branch_names: List[str] = list(filter(lambda x: x.startswith("* "), branch_response.get_stdout_lines()))
     if len(current_branch_names) != 1:
         return None
 
     local_branch_name: str = current_branch_names[0].removeprefix("* ")
-    tracking_branch_response: ShellResponse = dev_env.shell.run_or_raise("git",
-                                                                         [
-                                                                             "config",
-                                                                             "--get",
-                                                                             f"branch.{local_branch_name}.merge"
-                                                                         ],
-                                                                         dev_env.root)
+    tracking_branch_response: ShellResponse = dev_env.shell.run("git",
+                                                                [
+                                                                    "config",
+                                                                    "--get",
+                                                                    f"branch.{local_branch_name}.merge"
+                                                                ],
+                                                                dev_env.root)
+
+    if not tracking_branch_response.is_success:
+        return None
 
     tracking_branches: List[str] = list(filter(lambda x: len(x) > 0, tracking_branch_response.get_stdout_lines()))
     if len(tracking_branches) != 1:
@@ -109,13 +115,16 @@ def _get_current_branch(dev_env: DevelopmentEnvironment) -> Optional[CurrentLoca
 
     tracking_branch_name: str = tracking_branches[0].removeprefix("refs/heads/")
 
-    tracking_remote_response: ShellResponse = dev_env.shell.run_or_raise("git",
-                                                                         [
-                                                                             "config",
-                                                                             "--get",
-                                                                             f"branch.{local_branch_name}.remote"
-                                                                         ],
-                                                                         dev_env.root)
+    tracking_remote_response: ShellResponse = dev_env.shell.run("git",
+                                                                [
+                                                                    "config",
+                                                                    "--get",
+                                                                    f"branch.{local_branch_name}.remote"
+                                                                ],
+                                                                dev_env.root)
+
+    if not tracking_remote_response.is_success:
+        return None
 
     tracking_remotes: List[str] = list(filter(lambda x: len(x) > 0, tracking_remote_response.get_stdout_lines()))
     if len(tracking_remotes) != 1:
